4012228e1627f3030828e28d565a152241741a7a
ifonlob <ifonlob1108@g.educaand.es>
2026-05-11 10:45:38 +0200

    feat: main app actualizado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink], 
+  // Es vital importar RouterLink y RouterLinkActive para el menú
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  // Ahora este archivo está limpio de lógica de notas
 }
