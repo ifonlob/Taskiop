d6a95d4de604ee7d15eaa81c1b58388da738e154
ifonlob <ifonlob1108@g.educaand.es>
2026-05-11 14:02:04 +0200

    feat: añadida lógica detalles tarjeta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
-import { ListaNotasComponent } from './lista-notas/lista-notas'; 
-import { FormularioNotasComponent } from './formulario-notas/formulario-notas'; 
+import { ListaNotasComponent } from './lista-notas/lista-notas';
+import { FormularioNotasComponent } from './formulario-notas/formulario-notas';
+import { DetalleNotaComponent } from './detalles-notas/detalles-notas';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'lista', pathMatch: 'full' },  
+  { path: '', redirectTo: 'lista', pathMatch: 'full' },
   { path: 'lista', component: ListaNotasComponent },
   { path: 'formulario', component: FormularioNotasComponent },
-  { path: 'editar/:id', component: FormularioNotasComponent }
+  { path: 'editar/:id', component: FormularioNotasComponent },
+  // 2. Añadimos la ruta para el detalle
+  { path: 'detalle/:id', component: DetalleNotaComponent } 
 ];
