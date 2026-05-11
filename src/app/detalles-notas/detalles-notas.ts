076223e349b5ec77f623fa2bf81498ed3681e77e
ifonlob <ifonlob1108@g.educaand.es>
2026-05-11 13:40:13 +0200

    feat: añadida estructura inicial del componente

---
+++ src/app/detalles-notas/detalles-notas.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-notas',
+  imports: [],
+  templateUrl: './detalles-notas.html',
+  styleUrl: './detalles-notas.css',
+})
+export class DetallesNotas {}
