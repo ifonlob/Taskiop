0d91c49f2dfdcdc9df7fd7c9a63aa5c05c5c18b9
ifonlob <ifonlob1108@g.educaand.es>
2026-05-11 12:14:18 +0200

    feat: validación notas vacías

--- src/app/formulario-notas/formulario-notas.ts
+++ src/app/formulario-notas/formulario-notas.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core'; 
-import { FormsModule } from '@angular/forms'; 
-import { ActivatedRoute, Router, RouterLink } from '@angular/router'; 
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { NotasService } from '../servicios/notas-service';
 
 @Component({
   selector: 'app-formulario-notas',
   standalone: true,
-  imports: [FormsModule, RouterLink], 
+  imports: [FormsModule, RouterLink],
   templateUrl: './formulario-notas.html'
 })
 export class FormularioNotasComponent implements OnInit {
@@ -22,9 +22,11 @@ export class FormularioNotasComponent implements OnInit {
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
+    
     if (id) {
-      this.notaId = +id;
+      this.notaId = Number(id)
       const nota = this.notasService.obtenerNotaPorId(this.notaId);
+      
       if (nota) {
         this.titulo = nota.titulo;
         this.contenido = nota.contenido;
@@ -33,6 +35,11 @@ export class FormularioNotasComponent implements OnInit {
   }
 
   guardar() {
+    if (this.titulo.trim() === '' || this.contenido.trim() === '') {
+      alert('Por favor, rellena el título y el contenido.');
+      return; 
+    }
+
     if (this.notaId !== null) {
       this.notasService.actualizarNota(this.notaId, this.titulo, this.contenido);
     } else {
@@ -43,6 +50,7 @@ export class FormularioNotasComponent implements OnInit {
         fecha: new Date().toLocaleDateString()
       });
     }
+    
     this.router.navigate(['/lista']);
   }
 }
