41520b1903b02987335ad622349263a3f9e3b433
ifonlob <ifonlob1108@g.educaand.es>
2026-05-12 13:41:27 +0200

    feat: estructura de recordatorios añadida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { ListaNotasComponent } from './lista-notas/lista-notas';
 import { FormularioNotasComponent } from './formulario-notas/formulario-notas';
 import { DetalleNotaComponent } from './detalles-notas/detalles-notas';
+import { Recordatorio } from './recordatorio/recordatorio';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'lista', pathMatch: 'full' },
   { path: 'lista', component: ListaNotasComponent },
   { path: 'formulario', component: FormularioNotasComponent },
   { path: 'editar/:id', component: FormularioNotasComponent },
-  { path: 'detalle/:id', component: DetalleNotaComponent } 
+  { path: 'detalle/:id', component: DetalleNotaComponent } ,
+  { path: 'recordatorios', component: Recordatorio }
 ];
